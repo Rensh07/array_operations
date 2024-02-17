611dab5e9744f141e81b2df12cd931d463119bae
Rensh07 <renshpatel2005@gmail.com>
2024-02-17 12:23:02 +0530

    add file

--- script.js
+++ script.js
@@ -30,11 +30,18 @@ console.log(sum(arr1, arr2));
 const arr = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
 
 function ascending(arr) {
-  return arr.sort(function (a, b) {
-    return a - b;
-  });
+  const max = arr.length;
+  for (let i = 0; i < max - 1; i++) {
+    for (let j = 0; j < max - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
-
 console.log(ascending(arr));
 
 // Write a JavaScript function which takes two arrays as an argument and returns merged array in ascending order.
@@ -49,8 +56,24 @@ const arr3 = [3, 5, 1];
 const arr4 = [4, 2];
 
 function merge(arr3, arr4) {
-  x = arr3.concat(arr4);
-  return x.sort((a, b) => a - b);
+  const x = [];
+
+  for (let i = 0; i < arr3.length; i++) {
+    x.push(arr3[i]);
+  }
+  for (let i = 0; i < arr4.length; i++) {
+    x.push(arr4[i]);
+  }
+  for (let i = 0; i < x.length; i++) {
+    for (let j = i + 1; j < x.length; j++) {
+      if (x[i] > x[j]) {
+        const temp = x[i];
+        x[i] = x[j];
+        x[j] = temp;
+      }
+    }
+  }
+  return x;
 }
 
 console.log(merge(arr3, arr4));
@@ -66,14 +89,53 @@ console.log(merge(arr3, arr4));
 const a1 = [1, 2, 3, 4, 5, 6];
 const a2 = [3, 4, 5, 7];
 
-function merge(a1, a2) {
-  x = arr3.concat(arr4);
-  return x.sort((a, b) => a - b);
+function merge2(a1, a2) {
+  const y = [];
+
+  for (let i = 0; i < a1.length; i++) {
+    y.push(a1[i]);
+  }
+
+  for (let i = 0; i < a2.length; i++) {
+    y.push(a2[i]);
+  }
+
+  const ans = [];
+  for (let i = 0; i < y.length; i++) {
+    if (!ans.includes(y[i])) {
+      ans.push(y[i]);
+    }
+  }
+  return ans;
 }
 
-console.log(merge(a1, a2));
+console.log(merge2(a1, a2));
 
 // Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number.
 
 // Sample input: 025468
 // Expected output: 0-254-6-8.
+
+// Write a JavaScript program to find the most frequent item in an array.
+
+// Sample input : [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+// Expected Output : console.log(a, "5 times")
+
+const array = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+let mf = 1;
+let m = 0;
+let item;
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = i; j < array.length; j++) {
+    if (array[i] == array[j]) m++;
+    if (mf < m) {
+      mf = m;
+      item = array[i];
+    }
+  }
+  m = 0;
+}
+
+console.log(item + '  ' + mf + ' times ');
